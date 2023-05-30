dc15ae8c1107748138f8e5d50430c19965c47c46
ChristYoga123 <christianuswibisono@gmail.com>
2023-05-30 16:13:44 +0700

    feat: create migration and model

---
+++ app/Models/Venue.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Venue extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ["id"];
+
+    public function VenueCategory()
+    {
+        return $this->belongsTo(VenueCategory::class);
+    }
+
+    public function VenueGalleries()
+    {
+        return $this->hasMany(VenueGallery::class);
+    }
+}
